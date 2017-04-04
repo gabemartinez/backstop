abad0e9e1e1693b5f9a28e7c7bc8cbdf99097ad0
Gabriel Martinez <gabriel.martinez.2@asu.edu>
2017-04-04 16:18:06 -0700

    Initial Commit

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,330 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_0_header_0_phone.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_0_header_0_phone.png",
+        "selector": "header",
+        "fileName": "prod_test_CLAS_Homepage_0_header_0_phone.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "13.41",
+          "analysisTime": 13,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_0_header_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_1_ASUNavMenu_0_phone.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_1_ASUNavMenu_0_phone.png",
+        "selector": "#ASUNavMenu",
+        "fileName": "prod_test_CLAS_Homepage_1_ASUNavMenu_0_phone.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 57,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_2_hero-wrapper_0_phone.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_2_hero-wrapper_0_phone.png",
+        "selector": ".hero-wrapper",
+        "fileName": "prod_test_CLAS_Homepage_2_hero-wrapper_0_phone.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "15.17",
+          "analysisTime": 38,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_2_hero-wrapper_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_0_phone.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_0_phone.png",
+        "selector": "#block-mega-footer-mega-footer",
+        "fileName": "prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_0_phone.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 4
+          },
+          "misMatchPercentage": "7.26",
+          "analysisTime": 61,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_4_innovation-footer_0_phone.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_4_innovation-footer_0_phone.png",
+        "selector": "#innovation-footer",
+        "fileName": "prod_test_CLAS_Homepage_4_innovation-footer_0_phone.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 13,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_0_header_1_tablet_v.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_0_header_1_tablet_v.png",
+        "selector": "header",
+        "fileName": "prod_test_CLAS_Homepage_0_header_1_tablet_v.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "7.57",
+          "analysisTime": 15,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_0_header_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_1_ASUNavMenu_1_tablet_v.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_1_ASUNavMenu_1_tablet_v.png",
+        "selector": "#ASUNavMenu",
+        "fileName": "prod_test_CLAS_Homepage_1_ASUNavMenu_1_tablet_v.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 44,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_2_hero-wrapper_1_tablet_v.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_2_hero-wrapper_1_tablet_v.png",
+        "selector": ".hero-wrapper",
+        "fileName": "prod_test_CLAS_Homepage_2_hero-wrapper_1_tablet_v.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "22.59",
+          "analysisTime": 39,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_2_hero-wrapper_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_1_tablet_v.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_1_tablet_v.png",
+        "selector": "#block-mega-footer-mega-footer",
+        "fileName": "prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_1_tablet_v.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -4
+          },
+          "misMatchPercentage": "4.65",
+          "analysisTime": 100,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_1_tablet_v.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_4_innovation-footer_1_tablet_v.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_4_innovation-footer_1_tablet_v.png",
+        "selector": "#innovation-footer",
+        "fileName": "prod_test_CLAS_Homepage_4_innovation-footer_1_tablet_v.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 33,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_0_header_2_tablet_h.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_0_header_2_tablet_h.png",
+        "selector": "header",
+        "fileName": "prod_test_CLAS_Homepage_0_header_2_tablet_h.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -2
+          },
+          "misMatchPercentage": "9.88",
+          "analysisTime": 12,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_0_header_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_1_ASUNavMenu_2_tablet_h.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_1_ASUNavMenu_2_tablet_h.png",
+        "selector": "#ASUNavMenu",
+        "fileName": "prod_test_CLAS_Homepage_1_ASUNavMenu_2_tablet_h.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "4.80",
+          "analysisTime": 6,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_1_ASUNavMenu_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_2_hero-wrapper_2_tablet_h.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_2_hero-wrapper_2_tablet_h.png",
+        "selector": ".hero-wrapper",
+        "fileName": "prod_test_CLAS_Homepage_2_hero-wrapper_2_tablet_h.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "20.02",
+          "analysisTime": 86,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_2_hero-wrapper_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_2_tablet_h.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_2_tablet_h.png",
+        "selector": "#block-mega-footer-mega-footer",
+        "fileName": "prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_2_tablet_h.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -4
+          },
+          "misMatchPercentage": "3.44",
+          "analysisTime": 97,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170315-124625/failed_diff_prod_test_CLAS_Homepage_3_block-mega-footer-mega-footer_2_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CLAS_Homepage_4_innovation-footer_2_tablet_h.png",
+        "test": "../bitmaps_test/20170315-124625/prod_test_CLAS_Homepage_4_innovation-footer_2_tablet_h.png",
+        "selector": "#innovation-footer",
+        "fileName": "prod_test_CLAS_Homepage_4_innovation-footer_2_tablet_h.png",
+        "label": "CLAS Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 27,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
